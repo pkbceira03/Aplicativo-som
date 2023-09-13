c78dcbaabf128ca452903972e59fa998133be6ca
pkbceira03 <cabeceira2003@gmail.com>
2023-09-13 08:34:06 -0300

    limpando o projeto é instalando o react-native-slider

---
+++ components/MusicPlayer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const MusicPlayer = () =>{
+    return(
+        <View style={styles.container}> 
+            <Text>oiiiiii</Text>
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    container:{
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    }
+  });
+
+export default MusicPlayer;
---
+++ examples/data.js
@@ -0,0 +1,15 @@
+const music = [
+    {
+        title: 'War Pigs',
+        artist: 'Black Sabbath',
+        //image: require(),
+        id:1,
+    },
+
+    {
+        title: 'I still love you',
+        artist: 'KISS',
+        //Image: require(),
+        id:2,
+    }
+]
